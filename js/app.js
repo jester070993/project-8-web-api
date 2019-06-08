01704a1098bdf4a64a65a1782e132e423e7af5de
Jesse <jester070993@gmail.com>
2019-06-07 23:23:21 -0700

    js

--- js/app.js
+++ js/app.js
@@ -1,70 +1,44 @@
-console.log("ok");
-
-
 let data = document.getElementById('card1')
-// let img = document.getElementsByClassName('img1')
-// console.log(img)
+//create the 12 card variables from doc
 
-let randomuser1 = fetch("https://randomuser.me/api")
-let randomuser2 = fetch("https://randomuser.me/api")
-// let randomuser3 = fetch("https://randomuser.me/api")
-// let randomuser4 = fetch("https://randomuser.me/api")
-// let randomuser5 = fetch("https://randomuser.me/api")
-// let randomuser6 = fetch("https://randomuser.me/api")
-// randomuser1 = fetch("https://randomuser.me/api")
-// randomuser1 = fetch("https://randomuser.me/api")
-// randomuser1 = fetch("https://randomuser.me/api")
-// randomuser10 = "https://randomuser.me/api"
-// randomuser11 = "https://randomuser.me/api"
-// randomuser12 = "https://randomuser.me/api"
+//TO DO
+// create the 12 div card elements, name them card0 - crard 11
+//in js, create a loop that loops 12 times, and inside that loop
+// create a function that creates an img elemnt, appends it  to the inside card it is in,
+//gives it the resective class, and puts the correct innerHTML from the json response, 
+//incrementing i every loop so the function can access card[i] and give it the innerHTML
+//of reponse.results[i]...., or use a while loop - just any loop tht will loop 12 times
 
 
-// let randomusers =[fetch("https://randomuser.me/api"), fetch("https://randomuser.me/api"), fetch("https://randomuser.me/api"), fetch("https://randomuser.me/api"), 
-// fetch("https://randomuser.me/api"),fetch("https://randomuser.me/api"), fetch("https://randomuser.me/api"), fetch("https://randomuser.me/api"), fetch("https://randomuser.me/api")
-// ,fetch("https://randomuser.me/api"), fetch("https://randomuser.me/api"), fetch("https://randomuser.me/api")];
+//then create function that puts that information inside  modal box, for whichever card is clicked - click event listener, also probably use the 'tis '
 
-
-
-let randomusers =[];
-
-for( let i = 0; i < 3 ; i++) {
-	randomusers[i] = fetch("https://randomuser.me/api");
-}
- 
-Promise.all(randomusers)
-.then(users => {
-	return users.forEach((user) =>  console.log(user.json()))
+fetch("https://randomuser.me/api/?results=12")
+.then(data => {
+	return data.json();
 })
+.then(reponse => {
+	//replace this with function inside for loop, 12 times 
+	//newuser function example created at the bottom
+		let img = document.createElement("img")
+		data.appendChild(img)
+		img.classList.add("radius")
+		img.src = reponse.results[0].picture.thumbnail;
+		// data.innerHTML = `<ul class='card-list'>`
+		data.innerHTML += `<ul class='card-list'> 
+							<li> ${reponse.results[0].name.first} ${reponse.results[0].name.last} </li>
+							<li>  ${reponse.results[0].email} </li>							
+							<li>  ${reponse.results[0].email} </li>	
+							<li>  ${reponse.results[0].location.state} </li>	
+		`;
+		// data.innerHTML = data.innerHTML + "</ul>"
+		//same as... 
+		data.innerHTML += `</ul>`;
+		//fucntion demostation inside promise, newuser function exmaple, logs the specific data to the console
+		console.log(newuser(reponse.results[1].email + "you bitch!"))
+}) 
+//fucntion delcaration to use inside promise - example for testing purposes to make sure it works,
+let newuser = (user) => {
+	return user
+}
 
-	
-fetch("https://randomuser.me/api")
-.then(data => data.json())
-.then(response => console.log(response.results[0]))
-
-
-// fetch("https://randomuser.me/api")
-// .then(data => {
-// 	return data.json();
-// })
-// .then(reponse => {
-// 		let img = document.createElement("img")
-		// data.appendChild(img)
-
-// 		img.classList.add("radius")
-
-// 		img.src = reponse.results[0].picture.thumbnail;
-
-// 		// data.innerHTML = `<ul class='card-list'>`
-// 		data.innerHTML += `<ul class='card-list'> 
-// 							<li> ${reponse.results[0].name.first} ${reponse.results[0].name.last} </li>
-// 							<li>  ${reponse.results[0].email} </li>							
-// 							<li>  ${reponse.results[0].email} </li>	
-// 							<li>  ${reponse.results[0].location.state} </li>	
-// 		`;
-// 		// data.innerHTML = data.innerHTML + reponse.results[0].gender
-// 		data.innerHTML += `</ul>`;
-		
-// 		// data.innerHTML = data.innerHTML + "</h1>"
-// }) 
-
-
+newuser("hello!!!!");
