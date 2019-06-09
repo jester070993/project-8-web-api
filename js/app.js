7a6cd974025478c92f0eadfcfd1399680424d5bf
Jesse Dispoto <jester070993@gmail.com>
2019-06-08 20:01:51 -0700

    js basis, css styling

--- js/app.js
+++ js/app.js
@@ -1,44 +1,60 @@
-let data = document.getElementById('card1')
-//create the 12 card variables from doc
-
-//TO DO
-// create the 12 div card elements, name them card0 - crard 11
-//in js, create a loop that loops 12 times, and inside that loop
-// create a function that creates an img elemnt, appends it  to the inside card it is in,
-//gives it the resective class, and puts the correct innerHTML from the json response, 
-//incrementing i every loop so the function can access card[i] and give it the innerHTML
-//of reponse.results[i]...., or use a while loop - just any loop tht will loop 12 times
 
 
-//then create function that puts that information inside  modal box, for whichever card is clicked - click event listener, also probably use the 'tis '
+const users = document.getElementsByClassName("user");//returns array of html dom elements where info will be place
+const cards  = document.getElementsByClassName("card");
+const modal = document.getElementById("modalBox")
+let modalContent = document.getElementById("greyBox")
 
 fetch("https://randomuser.me/api/?results=12")
 .then(data => {
 	return data.json();
 })
 .then(reponse => {
-	//replace this with function inside for loop, 12 times 
-	//newuser function example created at the bottom
+
+	for (let i = 0 ; i < 12 ; i++){
 		let img = document.createElement("img")
-		data.appendChild(img)
-		img.classList.add("radius")
-		img.src = reponse.results[0].picture.thumbnail;
-		// data.innerHTML = `<ul class='card-list'>`
-		data.innerHTML += `<ul class='card-list'> 
-							<li> ${reponse.results[0].name.first} ${reponse.results[0].name.last} </li>
-							<li>  ${reponse.results[0].email} </li>							
-							<li>  ${reponse.results[0].email} </li>	
-							<li>  ${reponse.results[0].location.state} </li>	
+			users[i].appendChild(img)
+			img.classList.add("radius")
+			img.src = reponse.results[i].picture.thumbnail;
+		let firstName = reponse.results[i].name.first;
+		let lastName = reponse.results[i].name.last;
+		let firstChar = firstName.charAt(0).toUpperCase();
+		let lastChar = lastName.charAt(0).toUpperCase();
+			firstName = firstChar + firstName.slice(1);
+			lastName = lastChar + lastName.slice(1);
+		let location = reponse.results[i].location.state
+
+		let locationFirstChar = location.charAt(0).toUpperCase();
+			location = locationFirstChar + location.slice(1);
+		users[i].innerHTML +=  `<ul class='card-list'> 
+							<li class="name"> ${firstName} ${lastName} </li>
+							<li class="email">  ${reponse.results[i].email} </li>							
+							<li class="city">  ${location} </li>	
 		`;
-		// data.innerHTML = data.innerHTML + "</ul>"
-		//same as... 
-		data.innerHTML += `</ul>`;
+	}
 		//fucntion demostation inside promise, newuser function exmaple, logs the specific data to the console
 		console.log(newuser(reponse.results[1].email + "you bitch!"))
-}) 
+}) ;
 //fucntion delcaration to use inside promise - example for testing purposes to make sure it works,
 let newuser = (user) => {
 	return user
 }
 
 newuser("hello!!!!");
+
+Array.from(cards).forEach(card => { //"cards" returns an HTML nodelist collection, which is ARRAY-LIKE, but actaully an object - neeto use .form() to turn it into array
+	card.addEventListener("click", function(e){
+		console.log(e.target)
+		modal.style.display = "flex";
+		modal.addEventListener("click", function(e){
+			console.log(e.target.id)
+			if(e.target.tagName == "SPAN" || e.target.id == "modalBox"){
+				modal.style.display = "none";
+			}
+		})
+
+	})
+})
+
+
+
