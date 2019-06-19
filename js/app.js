e5a8a6f452f206d6fe897bab2476a70085771616
Jesse Dispoto <jester070993@gmail.com>
2019-06-19 16:22:38 -0700

    finished js, working as should

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
 
 const users = document.getElementsByClassName("user");//returns array-like object HTML collection of html dom elements where info will be place
 const cards  = document.getElementsByClassName("card");
-
 //modal variables
 const modal = document.getElementById("modalBox");
 let modalContent = document.getElementById("greyBox");
@@ -10,36 +9,40 @@ let modalContent = document.getElementById("greyBox");
 const nameOverlay = document.getElementsByClassName("nameOverlay")[0];
 const emailOverlay = document.getElementsByClassName("emailOverlay")[0];
 const overlayInfo = document.getElementsByClassName("overlayInfo")[0];
+let greyBox = document.querySelector("#greyBox");
+const extraInfo = document.querySelector(".extraInfo");
 
-let greyBox = document.querySelector("#greyBox")
-
-			const extraInfo = document.querySelector(".extraInfo")
-
-console.log(greyBox)
-
-console.log(nameOverlay, emailOverlay)
 
 fetch("https://randomuser.me/api/?results=20")
 .then(data => {
 	return data.json();
-})
+})  
 .then(reponse => {
 
 	for (let i = 0 ; i < 20 ; i++){
-		let img = document.createElement("img")
-			users[i].appendChild(img)
-			img.classList.add("radius")
+		let img = document.createElement("img");
+			users[i].appendChild(img);
+			img.classList.add("radius");
 			img.src = reponse.results[i].picture.thumbnail;
+			//capitalizes names
 		let firstName = reponse.results[i].name.first;
 		let lastName = reponse.results[i].name.last;
 		let firstChar = firstName.charAt(0).toUpperCase();
 		let lastChar = lastName.charAt(0).toUpperCase();
 			firstName = firstChar + firstName.slice(1);
 			lastName = lastChar + lastName.slice(1);
-		let location = reponse.results[i].location.state
 
-		let locationFirstChar = location.charAt(0).toUpperCase();
-			location = locationFirstChar + location.slice(1);
+			//capitalizes every word of location
+		let location = reponse.results[i].location.state;
+			location = location.split(" ").map(word =>	word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+			//ensures on the length of the dob is posted
+		let dob = reponse.results[i].dob.date.slice(0, 10);
+			//capitalizes every word for street data 
+		let street = reponse.results[i].location.street;
+		street = street.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+			//capitalizes every word for city
+		let city = reponse.results[i].location.city;
+			   city = city.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 
 		users[i].innerHTML +=  `<ul class='card-list '> 
 							<li class="name"> ${firstName} ${lastName} </li>
@@ -49,60 +52,31 @@ fetch("https://randomuser.me/api/?results=20")
 							<hr>
 							<ul class="extraInfo">
 								<li> ${reponse.results[i].phone}</li>
-								<li> ${reponse.results[i].phone}</li>
-								<li> ${reponse.results[i].phone}</li>
-
-							</ul>					
-		`;
-
-
-		// if (modal.style.display == "none"){
-			// console.log(reponse.results[i].phone)
-			// if (modal.style.display){
-			// 	console.log(reponse.results[i].phone)
-
-			// }
-	// <li class="phone">  ${reponse.results[i].phone} </li>
+								<li> ${street} ${city} ${location}, ${reponse.results[i].location.postcode} </li>
+								<li> DOB: ${dob}</li>
+								<div class="iconContainer"><span class="fas fa-times-circle icon"></span></div>
+							</ul>`;
 		
 	}
-		//fucntion demostation inside promise, newuser function exmaple, logs the specific data to the console
-		console.log(newuser(reponse.results[1].email + "you bitch!"))
 }) ;
-//fucntion delcaration to use inside promise - example for testing purposes to make sure it works,
-let newuser = (user) => {
-	return user
-}
-
-newuser("hello!!!!");
 
 Array.from(cards).forEach(card => { //"cards" returns an HTML nodelist collection, which is ARRAY-LIKE, but actaully an object - neeto use .form() to turn it into array
 	card.addEventListener("click", function(e){
 		// console.log(e.target)
 		modal.style.display = "flex";
 			greyBox.innerHTML += this.innerHTML;
-			// console.log(this.className)
-			console.log(greyBox.firstElementChild.className); 
-
 			greyBox.firstElementChild.className = "" ;
 			greyBox.firstElementChild.className = "givenLayout";
-			// greyBox.firstElementChild.classList.add = "givenLayout"
-
-			greyBox.lastElementChild.lastElementChild.style.display  = "block" ;
-
-
-			// this.style.display = "inline"
-			// greyBox.lastElementChild	// this.className = "cardLayout"
+			greyBox.lastElementChild.lastElementChild.style.display  = "block";
 		modal.addEventListener("click", function(e){
 			// console.log(e.target.id)
 			if(e.target.tagName == "SPAN" || e.target.id == "modalBox"){
 				modal.style.display = "none";
 				greyBox.innerHTML = "" ;
-				// this.className = "user"
 			}
-		})
-
-	})
-})
+		});
+	});
+});
 
 
 
